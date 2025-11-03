18243d80ba1bb948867c46a4c27c0068971d9537
kuokuo123 <kuoyu1204@gmail.com>
2025-11-03 16:37:17 +0800

    remove unecessary codes in helper scripts

--- contrib/external-editor-wrapper-niri.sh
+++ contrib/external-editor-wrapper-niri.sh
@@ -2,4 +2,3 @@
 niri msg action set-window-height 450 && niri msg action center-window
 nvim -n -c 'redraw!' '+normal G$' "$*"
 niri msg action set-window-height 60 && sleep 0.05 && niri msg action center-window
-exit
--- contrib/external-editor-wrapper-sway.sh
+++ contrib/external-editor-wrapper-sway.sh
@@ -2,4 +2,3 @@
 swaymsg [app_id=otter-launcher] resize set width 510 px height 280 px
 nvim -n -c 'redraw!' '+normal G$' "$*"
 swaymsg [app_id=otter-launcher] resize set width 510 px height 62 px
-exit
