e3d44be44da64942c5d49150ac955cd2b80e7e99
Tomáš Dreiseitl <dreistom@cvut.fit.cz>
2026-03-14 14:09:54 +0100

    make contrib scripts more portable

--- contrib/external-editor-wrapper-sway.sh
+++ contrib/external-editor-wrapper-sway.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 swaymsg [app_id=otter-launcher] resize set width 510 px height 280 px
 nvim -n -c 'redraw!' '+normal G$' "$*"
 swaymsg [app_id=otter-launcher] resize set width 510 px height 62 px
