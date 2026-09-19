2c836a8f1066c4e3301c8a1acc8420de8aecba05
kuokuo123 <kuoyu1204@gmail.com>
2026-09-20 00:47:42 +0800

    Revert "fix default_module_message and empty_module_message triggering unwanted line wrap when they have as many columns as the terminal window width"
    This reverts commit d764523df810d2b601222009393e886495100fb8.

--- src/helper.rs
+++ src/helper.rs
@@ -223,10 +223,12 @@ impl Highlighter for OtterHelper {
                     {
                         format!("\x1b[{}G{}{}", layout_right + 1, line, "\x1b[0m")
                     } else if index <= separator_count {
+                        //line.to_string()
                         format!("{}{}", line, "\x1b[0m")
                     } else if index > separator_count + selection_span
                         && FOOTER.get_or_init(|| String::new()).contains(line)
                     {
+                        //line.to_string()
                         format!("{}{}", line, "\x1b[0m")
                     } else {
                         let (part0, part_rest) =
@@ -559,7 +561,7 @@ impl Hinter for OtterHelper {
                                 0
                             };
                             // if empty module is set
-                            format!("\n\x1b[1G{}\r{}", e_module, "\n ".repeat(padded_line_count_local))
+                            format!("\n{}{}", e_module, "\n ".repeat(padded_line_count_local))
                         } else {
                             if agg_line.is_empty() {
                                 format!("{}", "\x1b[0m")
@@ -616,7 +618,7 @@ impl Hinter for OtterHelper {
                                 0
                             };
                             format!(
-                                "{}\n\x1b[1G{}\r{}{}",
+                                "{}\n{}{}{}",
                                 separator_lines,
                                 d_module,
                                 "\n ".repeat(padded_line_count_local),
